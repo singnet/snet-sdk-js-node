337598b0b89d50f40ae1e706f519a64d2a295a2c
Artash <artashes@vbrl.ai>
2025-01-17 10:15:48 +0300

    [SPT-726] Refactor ConcurrencyManager.js logs

--- src/ConcurrencyManager.js
+++ src/ConcurrencyManager.js
@@ -2,7 +2,7 @@ import * as grpc from '@grpc/grpc-js';
 // import { logger } from './core';
 import { TokenServiceClient } from './proto/token_service_grpc_pb';
 import { toBNString } from 'snet-sdk-core/utils/bignumber_helper';
-import { debug, error } from 'loglevel';
+import { logMessage } from 'snet-sdk-core/utils/logger';
 
 class ConcurrencyManager {
     /**
@@ -152,16 +152,16 @@ class ConcurrencyManager {
      */
     _getGrpcCredentials(serviceEndpoint) {
         if (serviceEndpoint.protocol === 'https:') {
-            debug('Channel credential created for https', { tags: ['gRPC'] });
+            logMessage('debug', 'ConcurrencyManager', 'Channel credential created for https');
             return grpc.credentials.createSsl();
         }
         if (serviceEndpoint.protocol === 'http:') {
-            debug('Channel credential created for http', { tags: ['gRPC'] });
+            logMessage('debug', 'ConcurrencyManager', 'Channel credential created for http');
             return grpc.credentials.createInsecure();
         }
 
         const errorMessage = `Protocol: ${serviceEndpoint.protocol} not supported`;
-        error(errorMessage, { tags: ['gRPC'] });
+        logMessage('error', 'ConcurrencyManager', errorMessage);
         throw new Error(errorMessage);
     }
 }
