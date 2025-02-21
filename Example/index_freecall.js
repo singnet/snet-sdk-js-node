ec98765f824fccb44d6bf64f879066830023bf83
Redarek <radmir.ibragimov@singularitynet.io>
2025-02-21 14:33:14 +0300

    [SPT-787] add concurrency and freecall examples

--- Example/index_freecall.js
+++ Example/index_freecall.js
@@ -16,6 +16,9 @@ const main = async () => {
 
     const opts = {
         disableBlockchainOperations: false,
+        tokenExpiryDateBlock: 10000000,
+        tokenToMakeFreeCall: "token",
+        email: "your@email.ru",
         concurrency: false
     };
 
